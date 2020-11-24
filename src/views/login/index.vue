42ffaaeb15403e0f3f977fa8aa63cbc24adca911
runoob <test@runoob.com>
2020-11-24 09:45:56 +0800

    login的美化操作

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="login-container">登录页面</div>
+  <div class="login-container">
+    <!--
+      el-form 表单组件
+      每个表单项都必须使用 el-form-item 组件包裹
+     -->
+
+    <div class="login-form-wrap">
+      <div class="login-head">
+        <div class="logo"></div>
+      </div>
+      <el-form class="login-form" ref="form" :model="user">
+        <el-form-item>
+          <el-input v-model="user.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="user.code" placeholder="请输入验证码"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox v-model="checked"
+            >我已阅读并同意用户协议和隐私条款</el-checkbox
+          >
+        </el-form-item>
+        <el-form-item>
+          <el-button class="login-btn" type="primary" @click="onSubmit"
+            >登录</el-button
+          >
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,14 +37,58 @@
     components: {},
     props: {},
     data() {
-      return {}
+      return {
+        user: {
+          mobile: '', // 手机号
+          code: '' // 验证码
+        },
+        checked: false // 是否同意协议的选中状态
+      }
     },
     computed: {},
     watch: {},
     created() {},
     mounted() {},
-    methods: {}
+    methods: {
+      onSubmit() {
+        console.log('submit!')
+      }
+    }
   }
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+  .login-container {
+    position: fixed;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background: url('./login_bg.jpg') no-repeat;
+    background-size: cover;
+    .login-form-wrap {
+      min-width: 300px;
+      padding: 30px 50px 10px;
+      background-color: #fff;
+      .login-head {
+        display: flex;
+        justify-content: center;
+        .logo {
+          width: 200px;
+          height: 57px;
+          background: url('./logo_index.png') no-repeat;
+          background-size: contain;
+        }
+      }
+      .login-form {
+        .login-btn {
+          width: 100%;
+        }
+      }
+    }
+  }
+</style>
