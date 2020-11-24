1f5947f7ba6850bf112dbcdc631c0a20636fe82f
runoob <test@runoob.com>
2020-11-24 20:11:34 +0800

    add baseurl

--- src/utils/request.js
+++ src/utils/request.js
@@ -10,7 +10,8 @@ import axios from 'axios'
 // 创建一个 axios 实例，说白了就是复制了一个 axios
 // 我们通过这个实例去发请求，把需要的配置配置给这个实例来处理
 const request = axios.create({
-  baseURL: 'http://ttapi.research.itcast.cn/' // 请求的基础路径
+  // baseURL: 'http://ttapi.research.itcast.cn/' // 请求的基础路径
+  baseURL: 'http://127.0.0.1:5000/'
 })
 
 // 请求拦截器
