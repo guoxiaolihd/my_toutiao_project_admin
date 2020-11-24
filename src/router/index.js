74e164efbc3fd609f3bc2055fecfcd0361ae86c2
runoob <test@runoob.com>
2020-11-24 14:29:26 +0800

    home01

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '@/views/login'
 import Home from '@/views/home'
+import Layout from '@/views/layout'
 
 Vue.use(VueRouter)
 
@@ -14,8 +15,15 @@ const routes = [
   },
   {
     path: '/',
-    name: 'home',
-    component: Home
+    name: 'layout',
+    component: Layout,
+    children: [
+      {
+        path: '',
+        name: 'home',
+        component: Home
+      }
+    ]
   }
 ]
 
