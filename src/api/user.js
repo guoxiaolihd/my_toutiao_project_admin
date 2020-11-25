8fb902ed8eaee0239c01451da5f87bc7633f140e
runoob <test@runoob.com>
2020-11-25 13:26:27 +0800

    add axios intercepor请求过滤器

--- src/api/user.js
+++ src/api/user.js
@@ -14,18 +14,10 @@ export const login = (data) => {
 }
 // 获取用户信息
 export const getUserProfile = () => {
-  const user = JSON.parse(window.localStorage.getItem('user'))
-
   return request({
     method: 'GET',
-    url: '/mp/v1_0/user/profile',
+    url: '/mp/v1_0/user/profile'
     // 后端要求把需要授权的用户身份放到请求头中
     // axios 可以通过 headers 选项设置请求头
-    headers: {
-      // 属性名和值都得看接口的要求
-      // 属性名：Authorization，接口要求的
-      // 属性值：Bearer空格token数据
-      Authorization: `Bearer ${user.token}`
-    }
   })
 }
